455238d99d052671b6eaf0ad37d73f07edae63e2
Ryan Dawkins <dawkinssu@gmail.com>
2014-10-10 13:57:45 -0500

    authoring project

---
+++ app/controllers/contact_controller.rb
@@ -0,0 +1,30 @@
+class ContactController < ApplicationController
+
+    respond_to :json
+
+    def new
+        data = person_params
+        if person_params
+            render json: Person.create(data)
+        else
+            render json: data
+        end
+    end
+
+    private
+    def person_params
+        begin
+            return params.permit(:first_name, :last_name, :email, :phone)
+        rescue
+            return nil
+        end
+    end
+
+    def error message
+        {
+            message: message,
+            params: params
+        }
+    end
+
+end
