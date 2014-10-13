b2afd32b627f803b49683220498272d14b32a332
Ryan Dawkins <dawkinssu@gmail.com>
2014-10-12 19:23:02 -0500

    Now requires presence of email, not just a format

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ActiveRecord::Base
     validates :first_name, presence: true
     validates :last_name, presence: true
-    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+    validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
