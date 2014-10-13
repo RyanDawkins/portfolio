ab20468b16957791797388acc811523941962ac8
Ryan Dawkins <dawkinssu@gmail.com>
2014-10-12 20:01:26 -0500

    Added error message to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  protected
+  def error message
+      {
+          message: message,
+          params: params
+      }
+  end
+
 end
