9043754253b7bf413ea48f57073b66df7ff0244e
Ryan Dawkins <dawkinssu@gmail.com>
2014-11-19 00:27:14 -0600

    Rework

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,66 +1,7 @@
-$(document).ready(function(e){
+app.controller("IndexController", ["$scope", '$anchorScroll', '$location', function($scope, $anchorScroll, $location){
 
-    // For some reason page is scrolling to bottom, will fix later.
-    window.scrollTo(0, 0);
-});
+  $scope.scroll = function(hash) {
+    $('html,body').animate({scrollTop: $("#"+hash).offset().top},'slow');
+  };
 
-$(document).on('click', '.nav-link', function(e){
-
-    // Changes what is considered 'active'
-    $('.nav-link').removeClass('active');
-    $(this).addClass('active');
-});
-
-$(document).on('click', 'a.scroll', function(e){
-
-    // Fixes flickering
-    e.preventDefault();
-
-    // Scrolls to that option
-    var href = $(this).attr("href");
-    if(href.charAt(0) === '#') {
-        $('html,body').animate({scrollTop: $(href).offset().top},'slow');
-    } else {
-        window.location.href = href;
-    }
-});
-
-$('#input-phone').formatter({
-    'pattern': '({{999}}) {{999}}-{{9999}}',
-    'persistent': true
-});
-
-$(document).on('submit', '#contact-form', function(event){
-
-    var first_name = $("#input-first-name").val();
-    var last_name = $("#input-last-name").val();
-    var email = $("#input-email").val();
-    var phone = $("#input-phone").val();
-
-    $.ajax({
-        type: "POST",
-        url: "/contact/create.json",
-        contentType: "application/json",
-        data: JSON.stringify({
-            first_name: first_name,
-            last_name: last_name,
-            email: email,
-            phone: phone
-        }),
-        dataType: 'json',
-        async: false,
-        success: function(data){
-            $("#contact-success").show("slow");
-        },
-        error: function(data){
-            $("#contact-error").show("slow");
-            console.log(data);
-        }
-    }).done(function(event){
-        setTimeout(function(){
-            $(".alert").hide("slow");
-        }, 5000);
-    });
-
-    return false;
-});
+}]);
